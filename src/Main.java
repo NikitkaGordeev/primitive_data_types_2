ad63c0e5ced9a967b9a165e55e6fed8db83e4e26
Nikita G <jobnikitka152rus@gmail.com>
2023-05-01 12:44:03 +0300

    bonus calculator

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int depositIn = 100;// начальный депозит
+        int amount = 100; // сумма пополнения
+        int bonus;
+        if(amount > 1000){ bonus = amount / 100;}else{
+            bonus = 0;
+        }
+        int balance = depositIn + amount + bonus;
+        System.out.println("Ваш баланс равен: " + balance + " руб");
+        System.out.println("Количество бонусов: " + bonus + " руб");
+    }
+}
